b15f5b951b5ac71f7ae633eb2caf855890962794
Roland-PC <rolandsalvador2000@gmail.com>
2021-11-11 21:05:06 -0800

    Configured NoteScroller script

---
+++ Assets/Scripts/NoteScroller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScroller : MonoBehaviour
+{
+    public float noteTempo;
+
+    public bool hasStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteTempo = noteTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(0f, noteTempo * Time.deltaTime, 0f);
+        }
+    }
+}
