bb5fbe9bceaebd54138975f41fb3e123055c8279
Roland-PC <rolandsalvador2000@gmail.com>
2021-11-12 14:42:34 -0800

    Added Prefabs folder for different hit timing effects (Hit, Good, Perfect, Miss)

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -8,6 +8,8 @@ public class NoteObject : MonoBehaviour
 
     public KeyCode keyToPress;
 
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,16 +31,19 @@ public class NoteObject : MonoBehaviour
                 {
                     Debug.Log("Hit");
                     GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
                 }
                 else if (Mathf.Abs(transform.position.y) > 0.05f)
                 {
                     Debug.Log("Good");
                     GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
                 }
                 else
                 {
                     Debug.Log("Perfect");
                     GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
                 }
             }
         }
@@ -61,6 +66,7 @@ public class NoteObject : MonoBehaviour
                 canBePressed = false;
 
                 GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
             }
         }
     }
