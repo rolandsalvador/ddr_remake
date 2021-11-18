9a628f16579791e4d72679273c012e49b87b6811
Roland-PC <rolandsalvador2000@gmail.com>
2021-11-18 03:19:09 -0800

    Need to fix movement of newly instantiated note prefabs

--- Assets/Scripts/NoteScroller.cs
+++ Assets/Scripts/NoteScroller.cs
@@ -8,6 +8,8 @@ public class NoteScroller : MonoBehaviour
 
     public bool hasStarted;
 
+    GameObject noteHolder;
+
     public GameObject leftArrow, downArrow, upArrow, rightArrow;
 
     // Start is called before the first frame update
@@ -17,14 +19,17 @@ public class NoteScroller : MonoBehaviour
         transform.position += new Vector3(0f, noteTempo * Time.deltaTime, 0f);
 
         Instantiate(leftArrow, new Vector3(-2.45f, -13, 0), Quaternion.identity);
+        noteHolder = GameObject.Find("NoteHolder");
+
+        //leftArrow.transform.parent = noteHolder.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasStarted == true)
-        {
-            //transform.position += new Vector3(0f, noteTempo * Time.deltaTime, 0f);
-        }
+        //if (hasStarted == true)
+        //{
+
+        //}
     }
 }
