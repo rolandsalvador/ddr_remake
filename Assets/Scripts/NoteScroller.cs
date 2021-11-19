8fa7906f47b7cc74c320d5daaf29dcd2cb62a241
Roland-PC <rolandsalvador2000@gmail.com>
2021-11-18 23:47:13 -0800

    Fixed instantiated note speed

--- Assets/Scripts/NoteScroller.cs
+++ Assets/Scripts/NoteScroller.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class NoteScroller : MonoBehaviour
 {
-    public float noteTempo;
-
     public bool hasStarted;
-
-    GameObject noteHolder;
 
     public GameObject leftArrow, downArrow, upArrow, rightArrow;
 
     // Start is called before the first frame update
     void Start()
     {
-        noteTempo = noteTempo / 60f;
-        transform.position += new Vector3(0f, noteTempo * Time.deltaTime, 0f);
-
-        Instantiate(leftArrow, new Vector3(-2.45f, -13, 0), Quaternion.identity);
-        noteHolder = GameObject.Find("NoteHolder");
-
-        //leftArrow.transform.parent = noteHolder.transform;
+        //Instantiate(leftArrow, new Vector3(-2.45f, -13, 0), Quaternion.identity);
+        SpawnLeftArrow();
+        SpawnDownArrow();
+        SpawnUpArrow();
+        SpawnRightArrow();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (hasStarted == true)
-        //{
+        
+    }
 
-        //}
+    public void SpawnLeftArrow()
+    {
+        Instantiate(leftArrow, new Vector3(-2.45f, -13, 0), Quaternion.identity);
+    }
+
+    public void SpawnDownArrow()
+    {
+        Instantiate(downArrow, new Vector3(-0.81f, -13, 0), Quaternion.identity);
+    }
+
+    public void SpawnUpArrow()
+    {
+        Instantiate(upArrow, new Vector3(0.81f, -13, 0), Quaternion.identity);
+    }
+
+    public void SpawnRightArrow()
+    {
+        Instantiate(rightArrow, new Vector3(2.44f, -13, 0), Quaternion.identity);
     }
 }
